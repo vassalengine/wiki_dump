924728b97ac6ef4c90be4f3fe424267cfce238c2
Joel Uckelman <uckelman@nomic.net>
2024-08-16 23:03:48 +0100

    Added more fixes.

--- fixup_json.py
+++ fixup_json.py
@@ -16,6 +16,16 @@ def collapse_pkgs(p):
     p['modules'] = { p['title']: d }
 
 
+def x13_dead_end_drive(p):
+    p['modules'] = {
+        'Spanish': [ m for v in p['modules'].values() for m in v ]
+    }
+
+
+def yggdrasil(p):
+    p['modules']['Dark Eclipse'] += p['modules'].pop('Dark Eclipse v2.0')
+
+
 def wacht_am_rhein(p):
     p['title'] = "'" + p['title']
     p['modules'] = {
@@ -31,12 +41,22 @@ def wacht_am_rhein(p):
 
 
 fixups = {
+    '13 Dead End Drive': x13_dead_end_drive,
     '1914': collapse_pkgs,
     '1936: Guerra Civil': collapse_pkgs,
     '300: Earth & Water': collapse_pkgs, 
     'A las Barricadas! (2nd Edition)': collapse_pkgs,
+    'Aces of Valor': collapse_pkgs,
+    'Across Suez': collapse_pkgs,
+    'Age of Dogfights: WW1': collapse_pkgs,
+    'Age of Muskets Volume I: Tomb for an Empire': collapse_pkgs,
     "Wacht am Rhein': The Battle of the Bulge, 16 Dec 44-2 Jan 45": wacht_am_rhein, 
-    'War At Sea': collapse_pkgs
+    'War At Sea': collapse_pkgs,
+    'The War At Sea (first edition)': collapse_pkgs,
+    'WARLINE: Maneuver Strategy & Tactics': collapse_pkgs,
+    'Ye Gods!': collapse_pkgs,
+    'Yggdrasil': yggdrasil,
+    'Zürich 1799': collapse_pkgs
 }
 
 #            # map each package individually 
